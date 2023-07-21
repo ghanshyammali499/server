b4aba69373df123da808be47028d5dda1c3963f6
ghanshyammali499 <ghanshyammali996@gmail.com>
2023-07-21 13:38:50 +0530

    first commit

--- server.js
+++ server.js
@@ -4,27 +4,27 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const connectDb = require("./config/connectDb");
-// config dot env file
+
 dotenv.config();
 
-//databse call
+
 connectDb();
 
-//rest object
+
 const app = express();
 
-//middlewares
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
-//routes
-app.use("/api/v1/users", require("./routes/userRoute"));
 
-//port
+app.use("/api/v1/users", require("./routes/userRoute"));
+app.use("/api/v1/transections", require("./routes/transectionRoutes"));
+
+
 const PORT = 8080 || process.env.PORT;
 
-//listen server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
